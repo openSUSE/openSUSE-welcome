f5b3b8827a83d9383b88f7ebe5b1f071bdc8c9e5
firnsy <firnsy@kororaproject.org>
2017-08-02 22:00:00 +1000

    updated: testing for k26 animated splash.

--- upstream/data/js/app.js
+++ upstream/data/js/app.js
@@ -140,4 +140,25 @@ $(document).ready( function() {
 
   /* initial call to page resize helper */
   setTimeout(function() { resizeHelper(); }, 0);
+
+  setTimeout(function() {
+    var alphaStart = 1;
+    var scaleFactor = 4 // by volume with 8 circs at 16px rad
+    var initialScale = 0.3;
+    var finalScale = initialScale*scaleFactor;
+    var offset = 0.1;
+    var travel = 16;
+
+    TweenMax.set('svg', { visibility: 'visible' });
+    TweenMax.set('.ellipse', { transformOrigin: "50% 50%" });
+
+    var tl = new TimelineMax({
+      repeat: 0,
+      onComplete: function() { $('#splash').fadeOut('slow'); }
+    });
+
+    tl.fromTo('.ellipses', 3.4, { x: -(travel) }, { x: travel, ease: Linear.easeNone })
+      .staggerFromTo('.ellipse', 2.5, { x: -350, autoAlpha: alphaStart }, { x: 350, autoAlpha: 1, ease: SlowMo.ease.config(0.25, 1.0, false), transformOrigin: "50% 50%"}, offset, "-=2.4")
+      .staggerFromTo('.ellipse', 1.75, { scale: initialScale }, { scale: finalScale, ease: SlowMo.ease.config(0, 1, true), yoyo: true}, offset, "-=3.0")
+  }, 0);
 });
