c1051287503935ba931a4d0ca414ba0d85570ad6
Carson Black <uhhadd@gmail.com>
2019-06-20 12:36:47 -0400

    comment out debug message printing

--- main.cpp
+++ main.cpp
@@ -51,7 +51,7 @@ void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QS
 
 int main(int argc, char *argv[])
 {
-    qInstallMessageHandler(myMessageOutput);
+    // qInstallMessageHandler(myMessageOutput);
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
