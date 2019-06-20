c9a1b919b4b38dae0d9431e838ba06002e08d494
Carson Black <uhhadd@gmail.com>
2019-06-20 12:30:17 -0400

    stick some copyright stickers on files

--- sysinfo.cpp
+++ sysinfo.cpp
@@ -1,25 +1,21 @@
 /*
-#include "sysinfo.h"
+    openSUSE Welcome - A welcome utility
+    Copyright (C) 2019  Carson Black
 
-SysInfo::SysInfo(QObject *parent) : QObject(parent), m_process(new QProcess(this))
-{
-    return;
-}
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-QString SysInfo::getOS()
-{
-    return QSysInfo::prettyProductName();
-}
-QString SysInfo::getArch()
-{
-    return QSysInfo::currentCpuArchitecture();
-}
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-SysInfo::~SysInfo()
-{
-
-}
-*/
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+ 
 #include "sysinfo.h"
 
 SysInfo::SysInfo(QObject *parent) :
