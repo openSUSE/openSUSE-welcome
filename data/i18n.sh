901afa9f571c5c5bfb8771aa97987518836dbce7
Carson Black <uhhadd@gmail.com>
2019-08-24 15:04:08 -0400

    Tidy repo

--- data/i18n.sh
+++ data/i18n.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-QRC=./i18n.qrc
+QRC=./data/i18n.qrc
 echo '<!DOCTYPE RCC>' > $QRC
 echo '<RCC version="1.0">' >> $QRC
 echo '  <qresource prefix="/">' >> $QRC
 
-for a in $(find i18n -type f)
+for a in $(find data/i18n -type f)
 do
 	if grep -i "\.po" <<< "$a"; then
 		echo a: $a
@@ -12,11 +12,11 @@ do
 		echo b: $b
 		c=$(rev <<< "$b" | cut -c 4- | rev )
 		echo c: $c
-		lconvert-qt5 $a -o "i18n$c.qm"
+		lconvert-qt5 $a -o "data$c.qm"
 	fi
 done
 # for each files/folder in the folder "theFokderName"
-for a in $(find i18n)
+for a in $(find data/i18n)
 do
     grep -v "\.qm" <<< "$a" && continue
     # if this is not a folder
