118e42cdf284f5f5d245416b6799872805c850c7
Carson Black <uhhadd@gmail.com>
2019-08-24 15:35:11 -0400

    Remove unneeded .qrc generation

--- data/i18n.sh
+++ data/i18n.sh
@@ -1,29 +1,10 @@
 #!/usr/bin/env bash
-QRC=./data/i18n.qrc
-echo '<!DOCTYPE RCC>' > $QRC
-echo '<RCC version="1.0">' >> $QRC
-echo '  <qresource prefix="/">' >> $QRC
-
-for a in $(find data/i18n -type f)
+mkdir -p ${1}
+for a in $(find ${MESON_SOURCE_ROOT}/data/i18n -type f)
 do
 	if grep -i "\.po" <<< "$a"; then
-		echo a: $a
-		b=$(cut -c 5- <<< "$a")
-		echo b: $b
-		c=$(rev <<< "$b" | cut -c 4- | rev )
-		echo c: $c
-		lconvert-qt5 $a -o "data$c.qm"
+		filename=$(basename $a)
+		y=${filename%.*}
+		lconvert-qt5 $a -o "${1}/${y}.qm"
 	fi
 done
-# for each files/folder in the folder "theFokderName"
-for a in $(find data/i18n)
-do
-    grep -v "\.qm" <<< "$a" && continue
-    # if this is not a folder
-    if [ ! -d "$a" ]; then
-        echo '      <file>'$a'</file>' >> $QRC
-    fi
-done
-
-echo '  </qresource>' >> $QRC
-echo '</RCC>' >> $QRC
