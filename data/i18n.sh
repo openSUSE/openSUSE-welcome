90d79ddf66db46bfc179de99bd57e21fa3a8dfee
Carson Black <uhhadd@gmail.com>
2019-09-30 21:04:14 -0400

    Fix i18n not supporting destdir

--- data/i18n.sh
+++ data/i18n.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-mkdir -p ${MESON_BUILD_ROOT}${1}
+mkdir -p ${DESTDIR}${1}
 for a in $(find ${MESON_SOURCE_ROOT}/data/i18n -type f)
 do
 	if grep -i "\.po" <<< "$a"; then
 		filename=$(basename $a)
 		y=${filename%.*}
-		lconvert-qt5 $a -o "${MESON_BUILD_ROOT}${1}/${y}.qm"
+		lconvert-qt5 $a -o "${DESTDIR}${1}/${y}.qm"
 	fi
 done
